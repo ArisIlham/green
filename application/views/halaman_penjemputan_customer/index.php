d653c89190426ffbb336d5468535ab5da3435629
Liana049 <liana.118140049@student.iteera.ac.id>
2022-04-07 08:20:14 +0700

    halaman penjemputan barang customer

---
+++ application/views/halaman_penjemputan_customer/index.php
@@ -0,0 +1,57 @@
+<html>
+    <head><title>
+        Halaman penjemputan barang
+        </title>
+    <link rel="stylesheet" href="style_pemesanan_customer.css">
+        
+    </head>
+
+   
+
+    <body>
+
+    <div class="header">
+        
+        <h1>
+        GREEN LAUNDRY EXPRESS PRINGSEWU</h1>
+
+        <h3>
+        Melayani dengan Sepenuh Hati, Cepat, dan Handal</h3>
+
+
+     <main id="main">
+        <div class="penjemputan-page">
+                <h2>
+                    <p class="center">Penjemputan Barang <p>
+                </h2>
+            <div class="form">
+                <form id="Penjemputan-form" >
+                    <div class="form-group">
+                        <label>Nama</label> <input name="nama" type="text">
+                    </div> <br>
+                    <div class="form-group">
+                        <label>Alamat</label> <input name="Alamat" type="text">
+                    </div> <br>
+                    <div class="form-group">
+                        <label>No.Hp</label>  <input name="No.Hp" type="text">
+                    </div><br>
+                    <div class="form-group">
+                        <label>Jenis Barang</label>  <input name="Jenis Barang" type="text">
+                    </div><br>
+                    <div class="form-group">
+                        <label>Note Pemesanan</label> <input name="Note Pemesanan" type="text">
+                    </div><br>
+                    <div>
+                        <label>Waktu Penjemputan</label> <input type="date" name="Waktu penjemputan">
+                    </div><br>
+
+                    <center><button class="btn-sm btn-block btn-wave col-md-4 btn-primary">Login</button></center>
+                </form>
+            </div>
+        </div>
+    </main>
+
+    <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+</body>
+</html>
